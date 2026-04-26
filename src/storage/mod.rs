2bef7e353a4f518912c65b3e240f266e43a9c59e
timlock <tim-lock@hotmail.de>
2026-04-26 16:32:39 +0200

    fix off by one error when parsing sstable len add test

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,25 +1,37 @@
 use std::array::TryFromSliceError;
 use std::collections::BTreeMap;
+use std::fmt::{Display, Pointer};
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Read, Seek, Write};
 use std::num::ParseIntError;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::str::Utf8Error;
 use std::string::FromUtf8Error;
-use std::{io, mem};
+use std::{error, fmt, fs, io, mem};
 
 #[derive(Debug)]
 pub enum Error {
     Io(io::Error),
-    WrongMagicNumber(Vec<u8>),
     FromUtf8(FromUtf8Error),
     Utf8(Utf8Error),
     TryFromSliceError(TryFromSliceError),
-    Truncated,
-    InvalidLengthEncoding(u8),
-    ExpectedLength,
     ParseIntError(ParseIntError),
 }
+
+impl error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::FromUtf8(e) => e.fmt(f),
+            Error::Utf8(e) => e.fmt(f),
+            Error::Io(error) => write!(f, "IO error: {error}"),
+            Error::TryFromSliceError(e) => e.fmt(f),
+            Error::ParseIntError(e) => e.fmt(f),
+        }
+    }
+}
+
 impl From<io::Error> for Error {
     fn from(value: io::Error) -> Self {
         Error::Io(value)
@@ -53,14 +65,16 @@ pub struct Storage {
     mem_table: MemTable,
     manifest: Manifest,
     flush_threshold: usize,
+    directory_path: PathBuf,
 }
 
 impl Storage {
-    pub fn new(directory_path: &Path, flush_threshold: usize) -> Result<Storage, Error> {
+    pub fn new(directory_path: PathBuf, flush_threshold: usize) -> Result<Storage, Error> {
         let mut manifest_file = BufReader::new(
             OpenOptions::new()
                 .create(true)
                 .write(true)
+                .read(true)
                 .open(directory_path.join("manifest"))?,
         );
 
@@ -70,6 +84,7 @@ impl Storage {
 
         let table_names = str::from_utf8(buf.as_slice())?
             .split(',')
+            .filter(|s| !s.is_empty())
             .collect::<Vec<_>>();
 
         let mut tables = Vec::with_capacity(table_names.len());
@@ -87,6 +102,7 @@ impl Storage {
         }
 
         Ok(Storage {
+            directory_path,
             flush_threshold,
             mem_table: MemTable::default(),
             manifest: Manifest {
@@ -97,12 +113,12 @@ impl Storage {
         })
     }
 
-    pub fn read(&mut self, key: &str) -> Result<Option<String>, Error> {
+    pub fn get(&mut self, key: &str) -> Result<Option<String>, Error> {
         if let Some(value) = self.mem_table.inner.get(key) {
             return Ok(Some(value.clone()));
         }
-        for ss_table in self.manifest.tables.iter_mut() {
-            let value = ss_table.find(key)?;
+        for table in self.manifest.tables.iter_mut() {
+            let value = table.find(key)?;
             if let Some(value) = value {
                 return Ok(Some(value));
             }
@@ -111,7 +127,7 @@ impl Storage {
         Ok(None)
     }
 
-    pub fn write(&mut self, key: String, value: String) -> Result<(), Error> {
+    pub fn insert(&mut self, key: String, value: String) -> Result<(), Error> {
         self.mem_table.insert(key, value);
         if self.mem_table.inner.len() >= self.flush_threshold {
             self.flush()?;
@@ -121,16 +137,17 @@ impl Storage {
 
     fn flush(&mut self) -> Result<(), Error> {
         let table_id = self.manifest.next_id();
-        let mut file = File::create(format!("table_{}", table_id))?;
+        let table_path = self.directory_path.join(format!("table_{}", table_id));
+        let mut file = File::create(&table_path)?;
 
         let mem_table = mem::take(&mut self.mem_table);
         let bytes = Vec::<u8>::from(mem_table);
         file.write_all(bytes.as_slice())?;
-        let ss_table = SSTable {
+        let table = SSTable {
             id: table_id,
-            file: BufReader::new(file),
+            file: BufReader::new(File::open(table_path)?),
         };
-        self.manifest.tables.push(ss_table);
+        self.manifest.insert(table)?;
 
         Ok(())
     }
@@ -208,8 +225,8 @@ impl SSTable {
             return Ok(None);
         }
 
-        let a = buf[..n].try_into()?;
-        let key_len = usize::from_le_bytes(a);
+        let len_slice = buf[..n - 1].try_into()?;
+        let key_len = usize::from_le_bytes(len_slice);
         Ok(Some(key_len))
     }
 
@@ -240,4 +257,51 @@ impl Manifest {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+    use std::{error, fs};
 
+    struct TempDir {
+        path: PathBuf,
+    }
+
+    impl TempDir {
+        fn new(path: PathBuf) -> io::Result<TempDir> {
+            fs::create_dir_all(&path)?;
+            Ok(TempDir { path })
+        }
+    }
+
+    impl Drop for TempDir {
+        fn drop(&mut self) {
+            fs::remove_dir_all(&self.path).unwrap();
+        }
+    }
+    #[test]
+    fn populate() -> Result<(), Box<dyn error::Error>> {
+        let temp_dir = TempDir::new(Path::new("testdata").join("populate"))?;
+        let mut storage = Storage::new(temp_dir.path.clone(), 10)?;
+
+        for i in 0..100 {
+            storage.insert(i.to_string(), i.to_string())?;
+        }
+
+        for i in 0..100 {
+            let value = storage.get(i.to_string().as_str())?;
+            assert_eq!(Some(i.to_string()), value);
+        }
+
+        for i in 0..100 {
+            storage.insert(i.to_string(), (i * 2).to_string())?;
+        }
+
+        for i in 0..100 {
+            let value = storage.get(i.to_string().as_str())?;
+            assert_eq!(Some((i*2).to_string()), value);
+        }
+
+        Ok(())
+    }
+}
